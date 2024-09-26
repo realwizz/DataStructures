c091542b136e40cb1ec684a2a6c2e80d7e454624
realwizz <womiowuna@gmail.com>
2024-09-26 11:36:37 +0100

    Added @someFloat' to output  to 3dp.

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -12,6 +12,10 @@ namespace DataStructures
         {
             Console.WriteLine("Hello, World!");
             Console.ReadKey();
+
+            float someFloat = 3.14159f;
+            Console.WriteLine(someFloat.ToString("0.00"));
+            Console.ReadKey();
         }
     }
 }

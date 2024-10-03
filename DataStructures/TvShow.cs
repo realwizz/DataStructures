727d47c00d250497f57c41cf70ede661eebc8a91
realwizz <womiowuna@gmail.com>
2024-10-03 11:35:41 +0100

    created a movie program

---
+++ DataStructures/TvShow.cs
@@ -0,0 +1,25 @@
+﻿using static DataStructures.Program;
+
+namespace DataStructures
+{
+    internal class TVShow
+    {
+        public string title = "Untitled";
+        public int releaseYear;
+        public double price;
+        public MovieRating rating;
+
+        public TVShow(string title, int releaseYear, double price MovieRating rating)
+        {
+            this.title = title;
+            this.releaseYear = releaseYear;
+            this.price = price;
+            this.rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return title + " (" + releaseYear + ") Rated" + rating + "...£" + price.ToString("0.00");
+        }
+    }
+}
